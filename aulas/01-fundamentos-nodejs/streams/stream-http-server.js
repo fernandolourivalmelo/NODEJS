fec7129cde9f5da2966c39cd061d285776f3a0e7
Fernando <62620873+fernandolourivalmelo@users.noreply.github.com>
2024-04-25 22:07:55 -0300

    exercicios
    exercicios do dia

--- aulas/01-fundamentos-nodejs/streams/stream-http-server.js
+++ aulas/01-fundamentos-nodejs/streams/stream-http-server.js
@@ -11,8 +11,20 @@ class InverseNumberStream extends Transform {
 
 //req - readablestream
 //res = writeblastream
-const server  = http.createServer((req,res)=>{
-    return req.pipe(new InverseNumberStream()).pipe(res)   
+const server  = http.createServer(async (req,res)=>{
+    const buffers = []
+
+    for await (const chunk of req ){
+        buffers.push(chunk)
+    }
+
+    const fullStreamContent = Buffer.concat(buffers).toString()
+
+    console.log(fullStreamContent)
+    return res.end(fullStreamContent)
+    // req.pipe(new InverseNumberStream()).pipe(res)   
+
+
 })
 
 server.listen(3334)
