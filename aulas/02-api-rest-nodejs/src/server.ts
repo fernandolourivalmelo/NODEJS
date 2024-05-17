6aae4123e296940573f846f7a513919c6565a9a6
Fernando <62620873+fernandolourivalmelo@users.noreply.github.com>
2024-05-16 21:30:50 -0300

    estudos 16/05/2024
    continuação do estudos

--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -1,14 +1,31 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import { title } from 'process'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello word'
+app.get('/hello', async () => {
+  // const tables = await knex('sqlite_schema').select('*')
+  // return tables
+
+  // const transaction = await knex('transactions').insert({
+  //   id: crypto.randomUUID(),
+  //   title: 'Transação de teste',
+  //   amount: 1000,
+  // })
+
+  // return transaction
+
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(function () {
     console.log('HTTP  Server Runing!')
