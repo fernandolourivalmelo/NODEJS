839065b2cad42cce96fe5171f6463538f79299bb
Fernando <62620873+fernandolourivalmelo@users.noreply.github.com>
2024-05-22 21:47:46 -0300

    estudos 22/05/2024
    estudos

---
+++ aulas/02-api-rest-nodejs/routes/transactions.ts
@@ -0,0 +1,44 @@
+/* eslint-disable prettier/prettier */
+import { FastifyInstance } from 'fastify'
+import { knex } from '../src/database'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createTransactionSchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionSchema.parse(request.body)
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
+  })
+
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return  {transactions} 
+  })
+
+  app.get('/:id',async(request)=>{
+    const getTransactionParamSchema = z.object({
+      id: z.string().uuid(), 
+    })
+
+    const {id} = getTransactionParamSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first() 
+
+    return {transaction}
+
+  })
+}
--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -3,24 +3,12 @@ import { knex } from './database'
 import { title } from 'process'
 import crypto from 'node:crypto'
 import { env } from './env'
+import { transactionsRoutes } from '../routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  // const tables = await knex('sqlite_schema').select('*')
-  // return tables
-
-  // const transaction = await knex('transactions').insert({
-  //   id: crypto.randomUUID(),
-  //   title: 'Transação de teste',
-  //   amount: 1000,
-  // })
-
-  // return transaction
-
-  const transactions = await knex('transactions').select('*')
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
