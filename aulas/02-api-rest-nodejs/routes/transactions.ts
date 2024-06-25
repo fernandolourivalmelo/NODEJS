1cf0537e44908e5f8cd75723e334fc83c018009d
Fernando <fldm.sc@gmail.com>
2024-06-24 21:32:42 -0300

    Update transactions.ts

--- aulas/02-api-rest-nodejs/routes/transactions.ts
+++ aulas/02-api-rest-nodejs/routes/transactions.ts
@@ -29,6 +29,12 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return  {transactions} 
   })
 
+  app.get('/summary',async()=>{
+    const summary = await knex ('transactions').sum('amount',{as:'amount'}).first()
+
+    return {summary}
+  })
+
   app.get('/:id',async(request)=>{
     const getTransactionParamSchema = z.object({
       id: z.string().uuid(), 
