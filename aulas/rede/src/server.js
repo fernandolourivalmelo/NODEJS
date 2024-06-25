1d55c93142ffa03d7ea3b2e5b5baf43c885802a0
Fernando <62620873+fernandolourivalmelo@users.noreply.github.com>
2024-06-25 19:37:35 -0300

    melhorias
    rotina de importação

--- aulas/rede/src/server.js
+++ aulas/rede/src/server.js
@@ -29,6 +29,16 @@ fastify.get('/importarConvenios', async (request, reply) => {
             console.log(descricao_area + '-' + id_rede)
         }
     )
+
+    const queryEnd = `SELECT guia_de_servico_enderecos.id_gdsend, guia_de_servico_enderecos.id_gds,  guia_de_servico_enderecos.id_referencia_endereco, guia_de_servico_enderecos.endereco, guia_de_servico_enderecos.numero, guia_de_servico_enderecos.complemento,  guia_de_servico_enderecos.bairro, guia_de_servico_enderecos.cd_cidade, a_cidades_1.Nm_cidade, guia_de_servico_enderecos.cep, guia_de_servico_enderecos.latitude,  guia_de_servico_enderecos.longitude, guia_de_servico_enderecos.telefone , guia_de_servico_enderecos.whatsapp, a_cidades_1.id_rede FROM [ASSOCIACAO].[cartao_beneficios].[dbo].guia_de_servico_enderecos as guia_de_servico_enderecos INNER JOIN  ASSOCIACAO.associacao.dbo.a_cidades AS a_cidades_1 ON  guia_de_servico_enderecos.cd_cidade = a_cidades_1.Cd_cidade  WHERE (guia_de_servico_enderecos.id_gds = `+ id_gds +`)`
+    const [resultadoEnd] = await sequelize.query(queryEnd)
+
+    resultadoEnd.forEach(async (enderecoConv)=>{
+        const {endereco,numero, complemento, bairro, Nm_cidade, cep, telefone , whatsapp, id_rede} = enderecoConv
+
+        console.log(endereco + '-' +  numero + '-' +  complemento + '-' +  bairro )
+
+    })
         
 
 
